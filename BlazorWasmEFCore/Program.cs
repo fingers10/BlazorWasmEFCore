5baa228c76d8d0ca52026a1bd87153a4ef6fdfe4
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-03-02 14:33:59 +0530

    Fix Sqlite Runtime Error - Could not find method 'AddYears' on type 'System.DateOnly'

--- BlazorWasmEFCore/Program.cs
+++ BlazorWasmEFCore/Program.cs
@@ -1,6 +1,7 @@
 using BlazorWasmEFCore;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Diagnostics.CodeAnalysis;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,3 +10,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+public partial class Program 
+{
+    /// <summary>
+    /// https://github.com/dotnet/efcore/issues/26860
+    /// https://github.com/dotnet/aspnetcore/issues/39825
+    /// FIXME: This is required for EF Core 6.0 as it is not compatible with trimming.
+    /// </summary>
+    [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
+    private static Type _keepDateOnly = typeof(DateOnly);
+}
